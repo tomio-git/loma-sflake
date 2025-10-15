0f155616660a679849b6e6e8cef8ae67d94739aa
tomio-git <tomi.oshode@bailliegifford.com>
2025-10-15 15:21:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered' + ', '+name_on_order+'!', icon="✅")
 
 
+# New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
